a472a103125b6cff77fa3be376b6f41906092a08
Jonathan Guyer <guyer@nist.gov>
2020-10-21 16:46:55 -0400

    Revert "Disable lots of the script"
    This reverts commit 6255e00ec8ab2fab81b5b3673cadbcb4ce60cb4d.

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -27,31 +27,31 @@ conda install -n base --quiet --yes conda-forge-ci-setup=3 conda-build pip
 
 
 
-# echo -e "\n\nSetting up the condarc and mangling the compiler."
-# setup_conda_rc ./ ./recipe ./.ci_support/${CONFIG}.yaml
-# mangle_compiler ./ ./recipe .ci_support/${CONFIG}.yaml
-# 
-# echo -e "\n\nMangling homebrew in the CI to avoid conflicts."
-# /usr/bin/sudo mangle_homebrew
-# /usr/bin/sudo -k
-# 
-# echo -e "\n\nRunning the build setup script."
-# source run_conda_forge_build_setup
-# 
-# 
-# if [[ ${CI} == "travis" ]]; then
-#   echo -en 'travis_fold:end:configure_conda\\r'
-# fi
-# 
-# set -e
-# 
-# echo -e "\n\nMaking the build clobber file and running the build."
-# make_build_number ./ ./recipe ./.ci_support/${CONFIG}.yaml
-# 
-# conda build ./recipe -m ./.ci_support/${CONFIG}.yaml --suppress-variables --clobber-file ./.ci_support/clobber_${CONFIG}.yaml ${EXTRA_CB_OPTIONS:-}
-# validate_recipe_outputs "${FEEDSTOCK_NAME}"
-# 
-# if [[ "${UPLOAD_PACKAGES}" != "False" ]]; then
-#   echo -e "\n\nUploading the packages."
-#   upload_package --validate --feedstock-name="${FEEDSTOCK_NAME}" ./ ./recipe ./.ci_support/${CONFIG}.yaml
-# fi
+echo -e "\n\nSetting up the condarc and mangling the compiler."
+setup_conda_rc ./ ./recipe ./.ci_support/${CONFIG}.yaml
+mangle_compiler ./ ./recipe .ci_support/${CONFIG}.yaml
+
+echo -e "\n\nMangling homebrew in the CI to avoid conflicts."
+/usr/bin/sudo mangle_homebrew
+/usr/bin/sudo -k
+
+echo -e "\n\nRunning the build setup script."
+source run_conda_forge_build_setup
+
+
+if [[ ${CI} == "travis" ]]; then
+  echo -en 'travis_fold:end:configure_conda\\r'
+fi
+
+set -e
+
+echo -e "\n\nMaking the build clobber file and running the build."
+make_build_number ./ ./recipe ./.ci_support/${CONFIG}.yaml
+
+conda build ./recipe -m ./.ci_support/${CONFIG}.yaml --suppress-variables --clobber-file ./.ci_support/clobber_${CONFIG}.yaml ${EXTRA_CB_OPTIONS:-}
+validate_recipe_outputs "${FEEDSTOCK_NAME}"
+
+if [[ "${UPLOAD_PACKAGES}" != "False" ]]; then
+  echo -e "\n\nUploading the packages."
+  upload_package --validate --feedstock-name="${FEEDSTOCK_NAME}" ./ ./recipe ./.ci_support/${CONFIG}.yaml
+fi
